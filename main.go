cf40986c9ec9040da0c9fe62b811c76786d68039
xieyuhua <1130107545@qq.com>
2024-03-15 09:50:20 +0800

    数据重连

--- main.go
+++ main.go
@@ -29,8 +29,7 @@ var db *sql.DB
 var dbh2 *gorm.DB
 var debug *bool
 
-//主函数
-func main() {
+func init (){
     debug   = flag.Bool("d", true, "debug msg")
     port    = flag.String("p", "8004", "服务端口")
     maxnum  = flag.Int("i", 1000, "SlidingWindow 100/30 s")
@@ -38,17 +37,23 @@ func main() {
     dbtype  = flag.String("t", "mysql", "mysql、sqlserver、oracle、clickhouse")
     dns     = flag.String("dns", "root:1b0ef5fde8798137@tcp(127.0.0.1:3306)/umami", "ClickHouse   tcp://127.0.0.1:42722?debug=false&database=azom_db&write_timeout=5&compress=true&username=default&password=password \nsqlserver    sqlserver://kangshu:bzynj@127.0.0.1:1433/?database=weixin&encrypt=disable \nMysql        root:root@tcp(127.0.0.1:3306)/test \nOracle       oracle://H2:hysoft@127.0.0.1:3521/hyee")
     flag.Parse()
-    // oracle,mysql,sqlsver
-	
-    app := fiber.New()
+}
+
+func initLog (){
+    //日志
+    logs.SetFormatter(&logrus.JSONFormatter{})
     
-    //
-    app.Use(limiter.New(limiter.Config{
-        Max:            *maxnum,
-        Expiration:     30 * time.Second,
-        LimiterMiddleware: limiter.SlidingWindow{},
-    }))
-    
+	logger := &lumberjack.Logger{
+		Filename:   "logrus.log",
+		MaxSize:    50,  // 日志文件大小，单位是 MB
+		MaxBackups: 3,    // 最大过期日志保留个数
+		MaxAge:     30,   // 保留过期文件最大时间，单位 天
+		Compress:   true, // 是否压缩日志，默认是不压缩。这里设置为true，压缩日志
+	}
+	logs.SetOutput(logger) // logrus 设置日志的输出方式
+}
+
+func initDb (){
 	//oracle://H2:hydeesoft@127.0.0.1:3521/hydee 
 	//root:ef08ef776ce21a44@tcp(127.0.0.1:3306)/after
 	//sqlserver://kangshu:bzdmmynj@127.0.0.1:1433/?database=weixin&encrypt=disable
@@ -60,18 +65,13 @@ func main() {
     	if err != nil {
     		panic(err)
     	}
-        sqlDB, err := dbh2.DB()
+        db, err = dbh2.DB()
     	if err != nil {
     		panic(err)
     	}
-        sqlDB.SetMaxOpenConns(50)
-        sqlDB.SetMaxIdleConns(10)
-        sqlDB.SetConnMaxLifetime(1800 * time.Second)
-        go func(){
-        	for range time.Tick(time.Duration(10) * time.Second) {
-        	    _ = sqlDB.Ping()
-    	    }
-        }() 
+        db.SetMaxOpenConns(50)
+        db.SetMaxIdleConns(10)
+        db.SetConnMaxLifetime(1800 * time.Second)
         // defer sqlDB.Close()
     } else {
         //数据库连接
@@ -83,35 +83,33 @@ func main() {
     	db.SetMaxIdleConns(10)//  设置最大空闲连接数, 该数值应该小于等于 SetMaxOpenConns 设置的值
     // 	db.SetConnMaxLifetime(8600)// 设置连接最大生命周期, 默认为 0(不限制), 我不建议设置该值, 只有当 mysql 服务器出现问题, 会导致连接报错, 恢复后可以自动恢复正常, 而我们配置了时间也不能卡住出问题的时间, 配置小还不如使用 SetConnMaxIdleTime 来解决
     	db.SetConnMaxIdleTime(1800 * time.Second) // 设置空闲状态最大生命周期, 该值应小于 mysql.wait_timeout 的值, 以避免被服务端断开连接, 产生报错影响业务， 一般可以配置 1天。
-        go func(){
-        	for range time.Tick(time.Duration(10) * time.Second) {
-        	    _ = db.Ping()
-    	    }
-        }()
     }
-    
-    
-    //日志
-    logs.SetFormatter(&logrus.JSONFormatter{})
-    
-	logger := &lumberjack.Logger{
-		Filename:   "logrus.log",
-		MaxSize:    50,  // 日志文件大小，单位是 MB
-		MaxBackups: 3,    // 最大过期日志保留个数
-		MaxAge:     30,   // 保留过期文件最大时间，单位 天
-		Compress:   true, // 是否压缩日志，默认是不压缩。这里设置为true，压缩日志
-	}
+}
 
-	logs.SetOutput(logger) // logrus 设置日志的输出方式
+//主函数
+func main() {
+    // oracle,mysql,sqlsver
+	//初始化
+    initDb()
     
-	
-	addr := fmt.Sprintf(":%v", *port)
-	log.Println("ip:port 0.0.0.0", addr)
-	log.Println(time.Now())
-	
+    initLog()
+    
+    //Ping  10
+    go func(){
+    	for range time.Tick(time.Duration(10) * time.Second) {
+    	    _ = db.Ping()
+	    }
+    }()
+        
+    app := fiber.New()
+    app.Use(limiter.New(limiter.Config{
+        Max:            *maxnum,
+        Expiration:     30 * time.Second,
+        LimiterMiddleware: limiter.SlidingWindow{},
+    }))
 
     app.Use("/", querySql)
-    app.Listen(addr)
+    app.Listen(fmt.Sprintf(":%v", *port))
 }
 
 type JsonRes struct {
@@ -134,7 +132,6 @@ func querySql(c *fiber.Ctx) error {
     if *allowip != "" {
         if !strings.Contains(strings.ToLower(*allowip), strings.ToLower(ip)) {
         	return c.JSON(JsonRes{Code: 4003, Msg: "  IP NOT ALLOW !"})
-        	
         }
     }
     
@@ -161,7 +158,27 @@ func querySql(c *fiber.Ctx) error {
                 "error": err,
                 "db": *dbtype,
             }).Info(sql_str)
-		    
+            
+            //当前连接是否需要重新连接
+             errors  := db.Ping()
+             if errors != nil {
+                if *dbtype == "oracle" {
+                    //数据库连接
+                	dbh2, _ = gorm.Open(oracle.Open(*dns), &gorm.Config{})
+                    db, _ = dbh2.DB()
+                    db.SetMaxOpenConns(50)
+                    db.SetMaxIdleConns(10)
+                    db.SetConnMaxLifetime(1800 * time.Second)
+                } else {
+                    //数据库连接
+                	db, _ = sql.Open(*dbtype, *dns)
+                	db.SetMaxOpenConns(50)
+                	db.SetMaxIdleConns(10)
+                	db.SetConnMaxIdleTime(1800 * time.Second)
+                }  
+             }
+            
+            c.JSON(JsonRes{Code: 4000, Msg: fmt.Sprintf(":%v", err)})
 			log.Println("查询sql发生错误", err)
 		}
 	}()
@@ -239,7 +256,6 @@ func querySql(c *fiber.Ctx) error {
     		resutData = append(resutData, colsjson)
     	}
     }
-	
-	defer rows.Close()	
-	return c.JSON(JsonRes{Code: 200, Data: resutData})
+    defer rows.Close()	
+    return c.JSON(JsonRes{Code: 200, Data: resutData})
 }
